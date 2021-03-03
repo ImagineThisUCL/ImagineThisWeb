21667ce5910066c61d83cddc80cbbb387f550e2b
Robin <bbaayezi@gmail.com>
2021-03-03 01:53:01 +0000

    remove uneeded code

--- src/App.js
+++ src/App.js
@@ -9,62 +9,6 @@ import NotFoundPage from "./views/NotFoundPage";
 import { userAPI } from "./api";
 
 function App() {
-  // when the user enters the root App page, initialise user credential and global states
-  const [state, dispatch] = useContext(FeedbackContext);
-  /**
-   * Create an user credential when the user first enters the homepage
-   */
-  const createAnonymousUser = async () => {
-    // create an anonymous user
-    console.log('Generating new user credential');
-    const userName = 'Anonymous User';
-    const userID = uuidv4();
-    // send a request to the server
-    await userAPI('POST', undefined, { userId: userID, userName })
-      .then((res) => {
-        if (res.data.success) {
-          console.log('Setting up local Storage');
-          // update localStorage
-          localStorage.setItem('user', JSON.stringify({ userID, userName }));
-          setUserCredential(userName, userID);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  /**
-   * This method will update the userName and userID in global context
-   */
-  const setUserCredential = (userName, userID) => {
-    dispatch({
-      type: "SET_USER_NAME",
-      payload: userName,
-    });
-    dispatch({
-      type: "SET_USER_ID",
-      payload: userID,
-    })
-  };
-
-  useState(() => {
-    // check if there is a user credential stored in localStorage
-    // if not, create a new user credential
-    const storedUserStr = localStorage.getItem('user');
-    let userID = "";
-    let userName = "";
-    if (storedUserStr === null) {
-      createAnonymousUser();
-    } else {
-      // get user credential from localStorage
-      const storedUser = JSON.parse(storedUserStr);
-      userID = storedUser.userID;
-      userName = storedUser.userName;
-      setUserCredential(userName, userID);
-    }
-  }, []);
-
   // FeedbackContextProvider provides the whole project with shared, centralised states
   return (
     // since the global context will be shared across different pages, the router should be a hash router
