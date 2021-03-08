4de960f6cf8c349a20490727b054efcd48173e20
Karish Singh <karish.singh28@gmail.com>
2021-03-08 19:58:47 +0000

    Added in intial version of updated QR tab with text box for Expo email.

--- src/components/project-tabs/QRTab.jsx
+++ src/components/project-tabs/QRTab.jsx
@@ -7,6 +7,12 @@ import "react-bootstrap";
 import moment from 'moment';
 import api from "../../api.js";
 import * as Icon from 'react-bootstrap-icons';
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import Search from "../../assets/Search.svg";
+
 
 const QRTab = (props) => {
   // useContext can be used to access global context and dispatch changes
@@ -42,7 +48,8 @@ const QRTab = (props) => {
   } else {
     console.log(`No conversions found for project ${state.projectID}`);
   }
-  
+  //Delete after testing.
+  lastConversion = "asdas"
 
   // Create QR code link
   const qrCodeLink = `exp://exp.host/@imaginethis/${state.projectID}`;
@@ -94,19 +101,42 @@ const QRTab = (props) => {
             {/* <p>In order to successfully run the prototype, please do the following steps</p> */}
             <ol className="pc-ordered-list">
               <p>To run this prototype on your device, do the following steps:</p>
-
               <li>
                 Install the "
                 <a href="https://expo.io/tools">Expo Go</a>
                 " app on your mobile device.
               </li>
               {/* <li>After opening the App, select the "Scan QR Code" option on the top of the sceen</li> */}
+              {/*<li>Sign into your Expo account, or create one if you don't already have one.</li>*/}
+              {/*<li>Add yourself to the ImagineThis Expo organisation by entering in your account's*/}
+                {/*email via the text box below</li>*/}
+              {/*<li>Go to your email and accept the invitation to the organisation.*/}
+              {/*  If you are already a member you can skip this step.</li>*/}
               <li>Open your device's built-in camera app and point it at the QR code on this page</li>
               <li>A notification will appear saying to open the build in Expo. Click on this.</li>
               <li>The expo app should then open and the prototype should begin to run on your device.</li>
             </ol>
             <div style={{textAlign: "center"}}>Last build: {moment(lastConversion.timestamp).format("DD/MM/YY HH:mm")} by {lastConversion.userName}</div>
           </div>
+
+          <Form
+            // onSubmit={this.handleSubmit}
+            className="input-group navbar-group"
+            style={{margin: "auto",width:"50%"}}
+          >
+            <InputGroup className="input-group-prepend">
+              <FormControl
+                className="form-control navbar-input"
+                aria-describedby="basic-addon1"
+                placeholder="Enter your Expo account email address"
+              />
+              <InputGroup.Append>
+                <Button variant="btn btn-light search-button" type="submit">
+                  <img alt="search button" src={Search}/>
+                </Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Form>
 
           <div className="clear" />
         </div>
@@ -150,6 +180,25 @@ const QRTab = (props) => {
               {' '}
               <a href="https://expo.io/">expo.io</a>
             </p>
+            <Form
+              // onSubmit={this.handleSubmit}
+              className="input-group navbar-group"
+              style={{margin: "auto",width:"90%"}}
+            >
+              <InputGroup className="input-group-prepend">
+                <FormControl style={{fontSize:"12px"}}
+                             className="form-control navbar-input"
+                             aria-describedby="basic-addon1"
+                             placeholder="Enter Expo Email Address"
+                />
+                <InputGroup.Append>
+                  <Button variant="btn btn-light search-button" type="submit">
+                    <img alt="search button" src={Search}/>
+                  </Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </Form>
+
           </div>
           <div className="mobile-qr">
             <a href={qrCodeLink}>
